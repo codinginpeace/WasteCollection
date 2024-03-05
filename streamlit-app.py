e6c6f6d9cb7a5ee03975a3bc8121f549ced2aa34
codinginpeace <elifnurozturk@sabanciuniv.edu>
2024-03-05 14:44:31 +0300

    initial streamlit page

---
+++ streamlit-app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+# Function to convert dataframe to CSV for download
+
+# Your existing Streamlit app setup
+st.set_page_config(
+    page_title="Waste Management Planning",
+    page_icon="⚡",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+
+st.header("Waste Management Dasboard")
+
